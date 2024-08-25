7d7e5ddaf3b743059fc7558417746c21da6250d8
Viktor Truderung <viktor@truderung.com>
2024-08-25 21:37:20 +0200

    the support for < 3.8 was dropped. Due to this the conditional import of metadata has been removed.

--- src/pyinstaller_versionfile/metadata.py
+++ src/pyinstaller_versionfile/metadata.py
@@ -15,19 +15,10 @@ except ImportError:  # pragma: no cover
 
 from pyinstaller_versionfile import exceptions
 
-import sys
-if sys.version_info[:2] >= (3, 8):
-    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
-    from importlib.metadata import (
-        PackageNotFoundError, 
-        distribution
-      )  # pragma: no cover
-else:
-    from importlib_metadata import (
-        PackageNotFoundError, 
-        distribution
-      )  # pragma: no cover
-
+from importlib.metadata import (
+    PackageNotFoundError, 
+    distribution
+)
 
 class MetaData(object):
     """
