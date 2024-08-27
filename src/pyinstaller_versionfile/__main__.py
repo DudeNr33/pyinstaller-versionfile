d29dbf0486db3513204ab69f4b7d94a8cda28938
Viktor Truderung <viktor@truderung.com>
2024-08-27 16:38:16 +0200

    changelog: new: cli extended for support of distribution metadata

--- src/pyinstaller_versionfile/__main__.py
+++ src/pyinstaller_versionfile/__main__.py
@@ -1,24 +1,46 @@
 """
 Main file for pyinstaller-versionfile, which is the entrypoint for the command line script.
 """
-
+from pathlib import Path
 import argparse
+import sys
 
+from pyinstaller_versionfile import exceptions
 import pyinstaller_versionfile
 
 
 def main(args=None):
     args = args or parse_args(args)
-    pyinstaller_versionfile.create_versionfile_from_input_file(
-        output_file=args.outfile,
-        input_file=args.metadata_file,
-        version=args.version
-    )
+    data = args.metadata_source
 
+    if args.source_format == "yaml":
+        # from_yaml
+        try:
+            src_file = Path(data)
+        except NotImplementedError as err:
+            raise exceptions.InputError(f"Path {src_file.as_posix()} is invalid") from err
+        else:
+            pyinstaller_versionfile.create_versionfile_from_input_file(
+                output_file=args.outfile,
+                input_file=src_file.as_posix(),
+                version=args.version
+            )
+            sys.exit(0)
+    elif args.source_format in ['distribution', 'dist']:
+        # from_distribution
+        pyinstaller_versionfile.create_versionfile_from_distribution(
+            output_file=args.outfile,
+            distname=data,
+            version=args.version
+        )
+        sys.exit(0)
+    else:
+        raise exceptions.InputError(f"Unknown source type {args.source_format}")
 
 def parse_args(args):
     parser = argparse.ArgumentParser(description="Create a version file for PyInstaller from a YAML metadata file.")
-    parser.add_argument("metadata_file", help="Path to YAML metadata file")
+    parser.add_argument("metadata_source", help="Either the path to the YAML metadata file or the name of the installed distribution.")
+    parser.add_argument("--source-format", default="yaml", help="Define the source format expected in metadata_source")
     parser.add_argument("--outfile", default="./version_file.txt", help="Resulting version file for PyInstaller")
     parser.add_argument("--version", default=None, help="Override Version information given in metadata file")
     return parser.parse_args(args)
