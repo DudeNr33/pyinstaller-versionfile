fc3256708f1c1eb23cfbfee529fcfdf1cf29e81b
Viktor Truderung <viktor@truderung.com>
2024-08-28 14:30:07 +0200

    change: due to diskussion try-except block removed and code adapted to new situation; optional arg --source-format equipped with valid choises; handling of internal usage error set

--- src/pyinstaller_versionfile/__main__.py
+++ src/pyinstaller_versionfile/__main__.py
@@ -1,7 +1,6 @@
 """
 Main file for pyinstaller-versionfile, which is the entrypoint for the command line script.
 """
-from pathlib import Path
 import argparse
 import sys
 
@@ -11,36 +10,38 @@ import pyinstaller_versionfile
 
 def main(args=None):
     args = args or parse_args(args)
-    data = args.metadata_source
-
     if args.source_format == "yaml":
         # from_yaml
-        try:
-            src_file = Path(data)
-        except NotImplementedError as err:
-            raise exceptions.InputError(f"Path {src_file.as_posix()} is invalid") from err
-        else:
-            pyinstaller_versionfile.create_versionfile_from_input_file(
-                output_file=args.outfile,
-                input_file=src_file.as_posix(),
-                version=args.version
-            )
-            sys.exit(0)
+        pyinstaller_versionfile.create_versionfile_from_input_file(
+            output_file=args.outfile,
+            input_file=args.metadata_source,
+            version=args.version
+        )
     elif args.source_format in ['distribution', 'dist']:
         # from_distribution
         pyinstaller_versionfile.create_versionfile_from_distribution(
             output_file=args.outfile,
-            distname=data,
+            distname=args.metadata_source,
             version=args.version
         )
-        sys.exit(0)
     else:
-        raise exceptions.InputError(f"Unknown source type {args.source_format}")
+        # because of 'choises' in --source-format this case should not be entered 
+        raise exceptions.InternalUsageError(f"Unexpected behaviour in main. Please check parser definition.")
+
+    sys.exit(0)
+
 
 def parse_args(args):
     parser = argparse.ArgumentParser(description="Create a version file for PyInstaller from a YAML metadata file.")
-    parser.add_argument("metadata_source", help="Either the path to the YAML metadata file or the name of the installed distribution.")
-    parser.add_argument("--source-format", default="yaml", help="Define the source format expected in metadata_source")
+    parser.add_argument(
+        "metadata_source",
+        help="Either the path to the YAML metadata file or the name of the installed distribution."
+    )
+    parser.add_argument(
+        "--source-format",
+        choices=["yaml", "distribution", "dist"],
+        default="yaml", help="Define the source format expected in metadata_source"
+    )
     parser.add_argument("--outfile", default="./version_file.txt", help="Resulting version file for PyInstaller")
     parser.add_argument("--version", default=None, help="Override Version information given in metadata file")
     return parser.parse_args(args)
